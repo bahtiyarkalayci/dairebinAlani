85e51dd0a677b647521b45497f30b12769ed57e9
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-10-29 19:32:56 +0300

    java101 pratik5 dairenin alanını bulma

---
+++ src/Alan.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Alan {
+    public static void main(String[] args) {
+        int r,centerArea;
+        double pi=3.14,alan;
+        Scanner input =new Scanner(System.in);
+        System.out.print("yarıçapı giriniz=");
+        r=input.nextInt();
+        System.out.print("merkez çapını giriniz=");
+        centerArea=input.nextInt();
+        alan=(pi*(r*r)*centerArea)/360;
+        System.out.print("daire diliminin alanı="+alan);
+
+    }
+}
