fd20203af40186f9ee471425bae4e619e413bfc0
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-10-29 19:51:16 +0300

    degisiklik yapıldı

--- src/Alan.java
+++ src/Alan.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Alan {
     public static void main(String[] args) {
         int r,centerArea;
-        double pi=3.14,alan;
+        double pi=3.14,alan,alan2;
         Scanner input =new Scanner(System.in);
         System.out.print("yarıçapı giriniz=");
         r=input.nextInt();
@@ -11,6 +11,7 @@ public class Alan {
         centerArea=input.nextInt();
         alan=(pi*(r*r)*centerArea)/360;
         System.out.print("daire diliminin alanı="+alan);
-
+        alan2=(pi*(r*r)*centerArea)/320;
+        System.out.println(alan2);
     }
 }
